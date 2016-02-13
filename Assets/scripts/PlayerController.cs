232caaf786c533f92b96f591ff5fa91161edf876
id-ilych <ily4@list.ru>
2016-02-13 11:39:15 +0200

    Initial commit

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+  public float speed;
+  public Text scoreLabel;
+  public Text winLabel;
+
+  private Rigidbody rigitBody;
+  private int score;
+
+  void Start() {
+    rigitBody = GetComponent<Rigidbody>();
+    score = 0;
+    validateScoreLabel();
+    winLabel.text = "";
+  }
+
+  void FixedUpdate() {
+    float moveHorizontal = Input.GetAxis("Horizontal");
+    float moveVertical = Input.GetAxis("Vertical");
+    Vector3 movement = speed * new Vector3(moveHorizontal, 0, moveVertical);
+    rigitBody.AddForce(movement);
+  }
+
+  void OnTriggerEnter(Collider other) {
+    if (other.gameObject.CompareTag("PickUp")) {
+      other.gameObject.SetActive(false);
+      ++score;
+      validateScoreLabel();
+      if (score >= 28) {
+        winLabel.text = "You win!";
+      }
+    }
+  }
+
+  private void validateScoreLabel() {
+    scoreLabel.text = "Score: " + score;
+  }
+}
